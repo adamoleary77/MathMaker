24f483f4ebba6c5e8bc903c2a8d36680b1db00d3
adamoleary77 <adamoleary77@gmail.com>
2019-05-23 23:56:01 -0400

    add files

---
+++ MathMaker.R
@@ -0,0 +1,71 @@
+library(shiny)
+
+### Server ###
+
+server <- function(input, output, session) {
+  #render header output from main text box
+  output$display <- renderUI({ 
+    withMathJax(
+      h4((paste0("> ", input$mainBox)))
+      )
+    })
+  
+  #Events for non-symbol-input buttons
+  #cat MathJax into main text
+  observeEvent(input$b1, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\theta\\)'))})
+  observeEvent(input$b2, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\pi\\)'))})
+  observeEvent(input$b3, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\epsilon\\)'))})
+  observeEvent(input$b4, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\delta\\)'))})
+  observeEvent(input$b5, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\ge\\)'))})
+  observeEvent(input$b6, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\le\\)'))})
+  observeEvent(input$b7, {updateTextInput(session, 'mainBox', value = paste0(input$mainBox, '\\(\\infty\\)'))})
+  #Events for symbol-input buttons
+  #cat MathJax into main & clear symbol input box
+  observeEvent(input$b8, {
+    x <- input$tempBox
+    updateTextInput(session,'mainBox', value = paste0(input$mainBox, '\\(\\sqrt{',x,'}\\)'))
+    updateTextInput(session,'tempBox', value = '')
+    })
+  observeEvent(input$b9, {
+    x <- input$tempBox
+    updateTextInput(session,'mainBox', value = paste0(input$mainBox, '$$^',x,'$$'))
+    updateTextInput(session,'tempBox', value = '')
+  })
+}
+
+### User Interface ###
+
+ui <- fluidPage(
+  #themeing and title
+  theme = "bootstrap_theme.css",
+  h1("Math Maker"),
+  #active text output
+  withMathJax(),
+  uiOutput('display'),
+
+  fluidRow(
+    #Input boxes column and helptext
+    column(4,
+      textInput("mainBox", "Main", width = '500px'),
+      textInput("tempBox", "Symbol Input", width = '100px'),
+      helpText("Type in the Main text box and your text will be translated to the output header."),
+      helpText("Use the Symbol Input text box for symbols that require values."),
+      helpText("ex) type 2 in the Symbol Input box then click the \\(\\sqrt{x}\\) button.")
+    ),
+    
+    #Buttons column
+    column(8,
+      #Symbol input buttons     
+      actionButton('b8', '\\(\\sqrt{x}\\)', width = '50px', style="padding:0px; font-size:200%"),
+      actionButton('b9', '$$x^e$$', width = '40px', style="padding:0px; font-size:200%"),
+      #No symbol input buttons
+      actionButton('b1', '\\(\\theta\\)', width = '50px', style="padding:0px; font-size:200%"),
+      actionButton('b2', '\\(\\pi\\)', width = '40px', style="padding:0px; font-size:200%"),
+      actionButton('b3', '\\(\\epsilon\\)', width = '40px', style="padding:0px; font-size:200%"),
+      actionButton('b4', '\\(\\delta\\)', width = '40px', style="padding:0px; font-size:200%"),
+      actionButton('b5', '\\(\\ge\\)', width = '40px', style="padding:0px; font-size:200%"),
+      actionButton('b6', '\\(\\le\\)', width = '40px', style="padding:0px; font-size:200%"),
+      actionButton('b7', '\\(\\infty\\)', width = '40px', style="padding:0px; font-size:200%")
+    )
+))
+shinyApp(ui, server)
